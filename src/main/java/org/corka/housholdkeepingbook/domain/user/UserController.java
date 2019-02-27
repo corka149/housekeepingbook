6a62d712b89a3aaa3879ec2c530f0504e22b063c
corka149 <corka149@mailbox.org>
2019-02-27 20:20:14 +0100

    Added users.html and table style

---
+++ src/main/java/org/corka/housholdkeepingbook/domain/user/UserController.java
@@ -0,0 +1,16 @@
+package org.corka.housholdkeepingbook.domain.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String getUsers() {
+        return "users";
+    }
+
+}
